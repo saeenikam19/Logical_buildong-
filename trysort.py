c48edb723918e4e803a738fe0aff48fc5090c1c8
saeenikam19 <saeenikam19@gmail.com>
2026-02-06 03:18:50 +1300

    Add files via upload

---
+++ trysort.py
@@ -0,0 +1,11 @@
+a=[22,3,76,4,9]
+
+sort=sorted(a,reverse=True)
+print(a)
+
+print(sort[0],"is a first heighst number. ")
+
+print(sort[1],"is a second heighst number. ")
+
+add=sort[0]+sort[1]
+print(add)
